98ec985c001b62566ca5aadb7cd381ce73c259f5
Kate <kate-ternovskaya99@mail.ru>
2024-08-13 10:57:56 +0300

    create todolist with CRUD

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {v1} from "uuid";
+import {useState} from "react";
+import Todolist from "./todolist/todolist";
 
+export type TaskType = {
+  id: string
+  title: string
+  isDone: boolean
+}
+
+export type FilterValuesType = 'all' | 'active' | 'completed'
 function App() {
+  const [tasks, setTasks] = useState<TaskType[]>([
+    {id: v1(), title: 'HTML&CSS', isDone: true},
+    {id: v1(), title: 'JS', isDone: true},
+    {id: v1(), title: 'ReactJS', isDone: false},
+  ])
+
+  const [filter, setFilter] = useState<FilterValuesType>('all')
+
+  const removeTask = (taskId: string) => {
+    const filteredTasks = tasks.filter((task) => {
+      return task.id !== taskId
+    })
+    setTasks(filteredTasks)
+  }
+
+  const addTask = (title: string) => {
+    const newTask = {
+      id: v1(),
+      title: title,
+      isDone: false
+    }
+    const newTasks = [newTask, ...tasks]
+    setTasks(newTasks)
+  }
+
+  const changeFilter = (filter: FilterValuesType) => {
+    setFilter(filter)
+  }
+
+  const changeTaskStatus = (taskId: string, taskStatus: boolean) => {
+    const newState = tasks.map((t: TaskType) => t.id == taskId ? {...t, isDone: taskStatus} : t)
+    setTasks(newState)
+  }
+
+  let tasksForTodolist = tasks
+  if (filter === 'active') {
+    tasksForTodolist = tasks.filter((task: TaskType) => !task.isDone)
+  }
+
+  if (filter === 'completed') {
+    tasksForTodolist = tasks.filter((task: TaskType) => task.isDone)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Todolist
+          title="What to learn"
+          tasks={tasksForTodolist}
+          removeTask={removeTask}
+          changeFilter={changeFilter}
+          addTask={addTask}
+          changeTaskStatus={changeTaskStatus}
+          filter={filter}
+      />
+
     </div>
   );
 }
